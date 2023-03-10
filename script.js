d3c081978d4ce4c65d0a68c89375a7da1fcc83ba
matute0 <matifernandezescuder@hotmail.com>
2023-03-10 14:05:30 -0300

    first JS

---
+++ script.js
@@ -0,0 +1,12 @@
+//Modify the variables a, b and c
+var a = 5;
+var b = 1;
+var c = -4;
+
+var delta = Math.sqrt((b * b - (4*a*c)));
+var result1 = ((-b + (delta)) / 2 * a);
+var result2 = ((-b - (delta)) / 2 * a);
+    
+    console.log("x1 = " + result1);
+    console.log("x2 = " + result2);
+   
